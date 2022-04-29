d2a286d61d2ef16374b2aaa7853e7aef54fce08f
Gaspard Mazataud <gaspard.mzt@gmail.com>
2022-04-29 05:05:30 +0200

    change book/:id to books/:id as Pierre Gentheon asked, already changed in the deployement on main by him

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.get('/book/:id', (req, res, next) => {
+app.get('/books/:id', (req, res, next) => {
   connection
     .promise()
     .query(
